a40c677e5de45a43ec7c9aebb433792fc6c9e2a2
Drew Lamph <drewlamph@gmail.com>
2021-05-25 12:24:31 +0100

    front end set up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Home from './components/common/Home'
+import Navbar from './components/common/Navbar'
+
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        {/* <Route path="/cheeses/:id" component={CheeseShow} />
+        <Route path="/cheeses" component={CheeseIndex} /> */}
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App
