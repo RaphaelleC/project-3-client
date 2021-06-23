144fa945cd1d8f8cac2674b8724ede1086eb2fd3
Drew Lamph <drewlamph@gmail.com>
2021-06-23 13:34:14 +0100

    updating with mapbox create new activity

--- src/components/hooks/useForm.js
+++ src/components/hooks/useForm.js
@@ -3,12 +3,13 @@ import React from 'react'
 export default function useForm(initialState) {
   const [formdata, setFormdata] = React.useState(initialState)
   const [formErrors, setFormErrors] = React.useState(initialState)
+
   const handleChange = e => {
-    setFormdata({ ...formdata, [e.target.name]: e.target.value })
+    setFormdata(formdata => ({ ...formdata, [e.target.name]: e.target.value }))
     setFormErrors({ ...formErrors, [e.target.name]: '' })
   }
 
-  const handleMultiSelect = selectedItems => { 
+  const handleMultiSelect = selectedItems => {
     const values = selectedItems ? selectedItems.map(item => item.value) : []
     setFormdata({ ...formdata, categories: values })
   }
